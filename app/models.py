4da56f18ae3e517a8899812df4bc3f22f2d5109b
Fozan Gardezi <fozangardezi@gmail.com>
2020-04-13 13:06:34 +0500

    Second Commit

--- app/models.py
+++ app/models.py
@@ -2,10 +2,9 @@ from app import db
 
 class Code(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
-	code = db.Column(db.String(1000),index = True)
+	code = db.Column(db.String(1000))
 
-	def __repr__(self):
-		return '<Code {}>'.format(self.code)
+
 
 	def __init__(self,code):
-		self.the_code = code
+		self.code = code
--- savecode.py
+++ savecode.py
@@ -4,3 +4,4 @@ from app.models import Code
 @app.shell_context_processor
 def make_shell_context():
 	return {'db':db, 'Code':Code}
+ 
